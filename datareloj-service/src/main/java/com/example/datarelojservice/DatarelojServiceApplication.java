e4f57e71fefb49d6bfaa9d8215288f99dcabad7b
Luis Javier Toro Flores <91231015+LuisToroSck@users.noreply.github.com>
2022-11-11 17:18:49 -0300

    Copia de microservicio en Maven

---
+++ datareloj-service/src/main/java/com/example/datarelojservice/DatarelojServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.datarelojservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatarelojServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatarelojServiceApplication.class, args);
+	}
+
+}
---
+++ datareloj-service/src/test/java/com/example/datarelojservice/DatarelojServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datarelojservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatarelojServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
